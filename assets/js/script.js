c60780b35ec72505711fa3cf2cf9740826d71e05
TI21-GALANG A.S <galang.ananda@raharja.info>
2024-09-26 14:06:00 +0700

    menambahkan  legalitas

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,18 @@ const dropdownmenu = document.querySelector(".dropdown-menu");
 dropdown.addEventListener("click", function () {
   dropdownmenu.classList.toggle("show");
 });
+
+// Padding navigasi berubah ketika di scroll
+
+window.onscroll = function () {
+  let navigasi = document.querySelector(".navbar");
+
+  if (
+    document.body.scrollTop > 120 ||
+    document.documentElement.scrollTop > 120
+  ) {
+    navigasi.classList.add("scroll");
+  } else {
+    navigasi.classList.remove("scroll");
+  }
+};
