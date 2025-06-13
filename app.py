6242359b00134659d6b7f97116bd5f8c4ee0b6c5
fatahillah2606 <muhamad.fatahillah@raharja.info>
2025-06-13 14:21:30 +0700

    Creating login page for admin

--- app.py
+++ app.py
@@ -12,15 +12,22 @@ def serve_node_modules(filename):
 collection = db_bce.use_db()
 data_chatbot = collection["chatbot"]
 
+
+# Customer Pages (Galang)
 # Home pages
 @app.route('/')
 def home():
   return render_template('index.html')
 
-# Chatbot Fadel
+# Admin Pagses (Fatah)
+@app.route("/admin")
+def admin():
+   return render_template('admin/index.html')
+
+# Chatbot (Fadel)
 @app.route('/chatbot')
 def chatbot():
-  return render_template('Test Chatbot (Alpha Test 1.0)/chatbot.html')
+  return render_template('chatbot/chatbot.html')
 
 # Perintah untuk chatbot
 @app.route('/commands')
