494131a8e8d5fcae2599e25afc3200bb0114a049
galangsktwn <galang.ananda@raharja.info>
2025-07-22 23:09:33 +0700

    banyak

--- myapp/customer/route.py
+++ myapp/customer/route.py
@@ -5,13 +5,32 @@ customer_route = Blueprint("customer", __name__)
 
 # Route
 @customer_route.route("/", methods=["GET", "POST"])
+
+# LOGIIN
 @customer_route.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("customer/login.html")
 
+# REGISTER
 @customer_route.route("/register", methods=["GET", "POST"])
 def register():
     return render_template("customer/register.html")
+
+#PENGECEKAN
+@customer_route.route("/pengecekan", methods=["GET", "POST"])
+def pengecekan():
+    return render_template("customer/pengecekan.html")
+
+# ONGKIR
+@customer_route.route("/ongkir", methods=["GET", "POST"])
+def ongkir():
+    return render_template("customer/ongkir.html")
+
+# PESANAN
+@customer_route.route("/pesanan", methods=["GET", "POST"])
+def pesanan():
+    return render_template("customer/pesanan.html")
+
 
 
 # Uji coba hal baru
