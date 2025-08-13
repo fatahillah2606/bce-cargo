772960c9948ac87f32f442034cf3a1d694b5be31
fatahillah2606 <muhamad.fatahillah@raharja.info>
2025-08-13 12:30:16 +0700

    feat: Creating admin page content
    - Creating content for several admin pages
    - Several design improvements for the admin pages

--- myapp/admin/route.py
+++ myapp/admin/route.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, jsonify, request, render_template, session, flash, redirect, url_for
+from itsdangerous import TimestampSigner, BadSignature, SignatureExpired
 from functools import wraps
 from connection import db_bce
 from bson.objectid import ObjectId
 from datetime import datetime, timedelta, timezone
-import bcrypt
+import bcrypt, math
 
 # Collection
 collection = db_bce.use_db()
@@ -11,18 +12,45 @@ userData = collection["users"]
 
 # API
 # Convert _id
-def convert_mongo_data(data):
-    return [{**item, "_id": str(item["_id"])} for item in data]
+def convert_objectid_to_str(doc):
+    if '_id' in doc:
+        doc['_id'] = str(doc['_id'])
+    return doc
 
 # Respon API
-def respon_api(status, code, message, data):
+def respon_api(status, code, message, data, pagination):
     respon = {
         "status": status,
         "code": code,
         "message": message,
-        "data": data if data else []
+        "data": data if data else [],
+        "pagination": pagination if pagination else {}
     }
     return jsonify(respon)
+
+# Tampilkan Semua Data
+def tampilkanSemuaData(koleksi, page, limit):
+    halaman = int(page if page else 1)
+    batas = int(limit if limit else 10)
+    lewati = (halaman - 1) * batas
+
+    total_items = koleksi.count_documents({})
+    total_halaman = math.ceil(total_items / batas)
+
+    cursor = koleksi.find({}).skip(lewati).limit(batas)
+    data = [convert_objectid_to_str(doc) for doc in cursor]
+
+    if data:
+        pageTest = {
+            "current_page": halaman,
+            "per_page": batas,
+            "total_items": total_items,
+            "total_pages": total_halaman
+        }
+
+        return respon_api("success", 200, str("Data tersedia"), data, pageTest)
+    else:
+        return respon_api("error", 404, str("Data tidak tersedia"), [], {})
 
 # Check duplicate entries
 def checkEntries(kunci, isi):
@@ -44,6 +72,16 @@ def khusus_admin(f):
 # Atur route
 admin_route = Blueprint("admin", __name__)
 
+@admin_route.context_processor
+def inject_globals():
+    return dict(
+        nama_pengguna=session.get("name"),
+        email_pengguna=session.get("email"),
+        role_pengguna=session.get("role"),
+        active_page=None  # ini bisa dioverride di masing-masing route
+    )
+
+
 # Route
 @admin_route.route("/")
 def login():
@@ -52,11 +90,53 @@ def login():
     else:
         return render_template("admin/auth/login.html")
 
+# Halaman
 @admin_route.route("/dashboard")
 @khusus_admin
 def dashboard():
-    return render_template("admin/pages/dashboard.html")
+    return render_template("admin/pages/dashboard.html", active_page="dashboard")
 
+@admin_route.route("/pesanan_masuk")
+@khusus_admin
+def pesanan_masuk():
+    return render_template("admin/pages/pesanan_masuk.html", active_page="pesanan_masuk")
+
+@admin_route.route("/invoice")
+@khusus_admin
+def invoice():
+    return render_template("admin/pages/invoice.html", active_page="invoice")
+
+@admin_route.route("/laporan")
+@khusus_admin
+def laporan():
+    return render_template("admin/pages/laporan.html", active_page="laporan")
+
+@admin_route.route("/manajemen_pengguna")
+@khusus_admin
+def manajemen_pengguna():
+    return render_template("admin/pages/manajemen_pengguna.html", active_page="manajemen_pengguna")
+
+@admin_route.route("/riwayat_pesanan")
+@khusus_admin
+def riwayat_pesanan():
+    return render_template("admin/pages/riwayat_pesanan.html", active_page="riwayat_pesanan")
+
+@admin_route.route("/status")
+@khusus_admin
+def status():
+    return render_template("admin/pages/status.html", active_page="status")
+
+@admin_route.route("/kategori")
+@khusus_admin
+def kategori():
+    return render_template("admin/pages/kategori.html", active_page="kategori")
+
+@admin_route.route("/keluhan")
+@khusus_admin
+def keluhan():
+    return render_template("admin/pages/keluhan.html", active_page="keluhan")
+
+# Logout
 @admin_route.route("/logout")
 def logout():
     session.clear()
@@ -78,29 +158,29 @@ def autentikasi():
                 # Cek entri duplikat
                 emailExist = checkEntries("email", email)
                 if emailExist:
-                    return respon_api("error", 409, "Email sudah ada", [])
+                    return respon_api("error", 409, "Email sudah ada", [], {})
                 
                 # Enkripsi sandi
-                pw = password.encode("utf-8")
-                garam = bcrypt.gensalt()
-                saltpass = bcrypt.hashpw(pw, garam)
+                hashedPass = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+                # decode ke string
+                hashedPass = hashedPass.decode("utf-8")
 
                 # Siapkan data
-
                 DataUser = {
                     "_id": ObjectId(),
                     "name": name,
                     "email": email,
-                    "password": saltpass,
+                    "password": hashedPass,
                     "role": role,
                     "regist_date": datetime.now()
                 }
 
                 # Masukan data ke dalam database
                 if userData.insert_one(DataUser):
-                    return respon_api("success", 200, "Data sent", [])
+                    return respon_api("success", 200, "Data sent", [], {})
                 else:
-                    return respon_api("error", 500, "Failed to sent", [])
+                    return respon_api("error", 500, "Failed to sent", [], {})
 
             # Sistem login
             if "login" in request.form:
@@ -111,6 +191,7 @@ def autentikasi():
                 akun = userData.find_one({"email": email, "role": "admin"})
                 if akun:
                     dbpw = akun["password"]
+                    dbpw = dbpw.encode("utf-8")
                     encodepw = password.encode("utf-8")
 
                     # validasi password
@@ -125,15 +206,50 @@ def autentikasi():
                         session["email"] = akun["email"]
                         session["role"] = akun["role"]
 
-                        return respon_api("success", 200, "Verifikasi berhasil", [])
+                        return respon_api("success", 200, "Verifikasi berhasil", [], {})
                     else:
-                        return respon_api("error", 401, "Verifikasi gagal", [])
+                        return respon_api("error", 401, "Verifikasi gagal", [], {})
                 else:
-                    return respon_api("error", 401, "Verifikasi gagal", [])
+                    return respon_api("error", 401, "Verifikasi gagal", [], {})
 
     except Exception as error:
-        return respon_api("error", 500, str(error), [])
-        
+        return respon_api("error", 500, str(error), [], {})
+
+# Pemesanan Kargo
+@admin_route.route("/api/data/pesanan", methods=["GET", "POST"])
+@khusus_admin
+def dataPesanan():
+    try:
+        if request.method == "GET":
+            pemesanan = collection["pemesanan_kargo"]
+            return tampilkanSemuaData(pemesanan, request.args.get("page", 1), request.args.get("limit", 10))
+
+    except Exception as error:
+        return respon_api("error", 500, str(error), [], {})
+    
+# Tarif
+@admin_route.route("/api/data/tarif", methods=["GET", "POST"])
+@khusus_admin
+def dataTarif():
+    try:
+        if request.method == "GET":
+            tarif = collection["tarif"]
+            return tampilkanSemuaData(tarif, request.args.get("page", 1), request.args.get("limit", 10))
+
+    except Exception as error:
+        return respon_api("error", 500, str(error), [], {})
+
+# Kategori Barang Kargo
+@admin_route.route("/api/data/kategori", methods=["GET", "POST"])
+@khusus_admin
+def dataKategori():
+    try:
+        if request.method == "GET":
+            kategori_barang = collection["kategori_barang_kargo"]
+            return tampilkanSemuaData(kategori_barang, request.args.get("page", 1), request.args.get("limit", 10))
+
+    except Exception as error:
+        return respon_api("error", 500, str(error), [], {})
 
 # Experiment
 @admin_route.route("/register", methods=["GET", "POST"])
