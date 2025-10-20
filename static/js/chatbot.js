2268435bb2814d811a7827453c4f64d0ee107250
fatahillah2606 <muhamad.fatahillah@raharja.info>
2025-10-20 19:56:26 +0700

    Improve the design of the Company Profile website for CV Bahtera Cahaya Express

--- static/js/chatbot.js
+++ static/js/chatbot.js
@@ -9,120 +9,165 @@ const API_URL = "/app/bce-cargo.chatbot.json"; // api data perusahaan
 let companyInfo; // Untuk menyimpan informasi perusahaan
 
 fetch(API_URL)
-  .then((res) => {
-    if (!res.ok) {
-      throw new Error(`HTTP error! Status: ${res.status}`);
-    }
-    return res.json();
-  })
-  .then((data) => (companyInfo = data))
-  .catch((error) => console.error("Unable to fetch data:", error));
+    .then((res) => {
+        if (!res.ok) {
+            throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        return res.json();
+    })
+    .then((data) => (companyInfo = data))
+    .catch((error) => console.error("Unable to fetch data:", error));
 
 // Pesan pengguna
 let userMessage;
 
 const inputInitHeight = chatInput.scrollHeight;
 
+// Convert Markdown
+function convertMarkdownLinksToHTML(text) {
+    // Link
+    text = text.replace(
+        /\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g,
+        '<a href="$2" target="_blank">$1</a>'
+    );
+
+    // Bold (**text**)
+    text = text.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>");
+
+    // Italic (*text* or _text_)
+    text = text.replace(/(?<!\*)\*(?!\*)(.*?)\*(?!\*)/g, "<em>$1</em>");
+    text = text.replace(/_(.*?)_/g, "<em>$1</em>");
+
+    // Underline (__text__)
+    text = text.replace(/__(.*?)__/g, "<u>$1</u>");
+
+    // Code block ```text```
+    text = text.replace(/```([\s\S]*?)```/g, "<pre><code>$1</code></pre>");
+    return text;
+}
+
+function fillTemplate(type) {
+    const textarea = document.querySelector(".chat-input textarea");
+    const sendBtn = document.getElementById("send-btn");
+
+    if (type === "keluhan") {
+        textarea.value = "Saya ingin membuat keluhan caranya bagaimana ya?";
+    } else if (type === "cek_pemesanan") {
+        textarea.value = "Saya ingin mengecek status pemesanan";
+    }
+
+    // Simulate click on send
+    sendBtn.click();
+}
+
 const createChatLi = (message, className) => {
-  // Membuat elemen <li> dengan pesan yang diteruskan dan className
-  const chatLi = document.createElement("li");
-  chatLi.classList.add("chat", className);
-  let chatContent =
-    className === "outgoing"
-      ? `<p></p>`
-      : `<span class="material-symbols-rounded">smart_toy</span><p></p>`;
-  chatLi.innerHTML = chatContent;
-  chatLi.querySelector("p").textContent = message;
-  return chatLi;
+    // Membuat elemen <li> dengan pesan yang diteruskan dan className
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", className);
+    let chatContent =
+        className === "outgoing"
+            ? `<p>${message}</p>`
+            : `<img src='/static/images/logo.png' alt='Logo'><p>${message}</p>`;
+    chatLi.innerHTML = chatContent;
+    chatLi.querySelector("p").textContent = message;
+    return chatLi;
 };
 
 const generateResponse = async (incomingChatLi) => {
-  const messageElement = incomingChatLi.querySelector("p");
+    const messageElement = incomingChatLi.querySelector("p");
 
-  // Cek apakah companyInfo sudah terisi
-  if (!companyInfo || Object.keys(companyInfo).length === 0) {
-    console.log("Company Info is empty");
-    messageElement.textContent = "Maaf, informasi perusahaan tidak tersedia.";
-    return;
-  }
+    // Cek apakah companyInfo sudah terisi
+    if (!companyInfo || Object.keys(companyInfo).length === 0) {
+        console.log("Company Info is empty");
+        messageElement.textContent =
+            "Maaf, informasi perusahaan tidak tersedia.";
+        return;
+    }
 
-  //Jawab pertanyaan client
-  companyInfo.forEach((jawaban, index, array) => {
-    let pertanyaan = jawaban["command"];
-    // Tangani jika pertanyaan dalam bentuk Array pada database
-    if (Array.isArray(jawaban["command"])) {
-      pertanyaan.forEach((kataKunci) => {
-        if (userMessage.toLowerCase().includes(kataKunci.toLowerCase())) {
-          prosesJawaban(array.indexOf(jawaban));
+    //Jawab pertanyaan client
+    companyInfo.forEach((jawaban, index, array) => {
+        let pertanyaan = jawaban["command"];
+        // Tangani jika pertanyaan dalam bentuk Array pada database
+        if (Array.isArray(jawaban["command"])) {
+            pertanyaan.forEach((kataKunci) => {
+                if (
+                    userMessage.toLowerCase().includes(kataKunci.toLowerCase())
+                ) {
+                    prosesJawaban(array.indexOf(jawaban));
+                }
+            });
+        } else if (
+            userMessage.toLowerCase().includes(pertanyaan.toLowerCase())
+        ) {
+            prosesJawaban(array.indexOf(jawaban));
         }
-      });
-    } else if (userMessage.toLowerCase().includes(pertanyaan.toLowerCase())) {
-      prosesJawaban(array.indexOf(jawaban));
+    });
+
+    function prosesJawaban(index) {
+        let responJawaban = companyInfo[index]["response"];
+        // Jika jawaban berupa array
+        if (Array.isArray(responJawaban)) {
+            messageElement.textContent = responJawaban
+                .map((item, index) => `${index + 1}. ${item}`)
+                .join("\n");
+
+            // Jika jawaban berupa Object
+        } else if (
+            typeof responJawaban === "object" &&
+            responJawaban !== null
+        ) {
+            messageElement.innerHTML = Object.entries(responJawaban)
+                .map(([key, value]) => `<b>${key}</b>: ${value}`)
+                .join("\n");
+
+            // Jika jawaban berupa String atau sejenisnya
+        } else {
+            messageElement.textContent =
+                responJawaban || "Maaf, aku tidak punya informasi tentang itu.";
+        }
     }
-  });
-
-  function prosesJawaban(index) {
-    let responJawaban = companyInfo[index]["response"];
-    // Jika jawaban berupa array
-    if (Array.isArray(responJawaban)) {
-      messageElement.textContent = responJawaban
-        .map((item, index) => `${index + 1}. ${item}`)
-        .join("\n");
-
-      // Jika jawaban berupa Object
-    } else if (typeof responJawaban === "object" && responJawaban !== null) {
-      messageElement.innerHTML = Object.entries(responJawaban)
-        .map(([key, value]) => `<b>${key}</b>: ${value}`)
-        .join("\n");
-
-      // Jika jawaban berupa String atau sejenisnya
-    } else {
-      messageElement.textContent =
-        responJawaban || "Maaf, aku tidak punya informasi tentang itu.";
-    }
-  }
-  // Scroll chatbox ke bawah setelah menampilkan pesan
-  chatbox.scrollTo(0, chatbox.scrollHeight);
+    // Scroll chatbox ke bawah setelah menampilkan pesan
+    chatbox.scrollTo(0, chatbox.scrollHeight);
 };
 
 const handleChat = async () => {
-  userMessage = chatInput.value.trim(); // Supaya gak ada spasi lebih
-  if (!userMessage) return;
-  chatInput.value = "";
-  chatInput.style.height = `${inputInitHeight}px`;
+    userMessage = chatInput.value.trim(); // Supaya gak ada spasi lebih
+    if (!userMessage) return;
+    chatInput.value = "";
+    chatInput.style.height = `${inputInitHeight}px`;
 
-  // Menambahkan pesan pengguna ke chatbox
-  chatbox.appendChild(createChatLi(userMessage, "outgoing"));
-  chatbox.scrollTo(0, chatbox.scrollHeight);
-
-  setTimeout(async () => {
-    // Menampilkan delay bot untuk merespon pengguna
-    const incomingChatLi = createChatLi(
-      "Maaf saya tidak mengerti :)",
-      "incoming"
-    );
+    // Menambahkan pesan pengguna ke chatbox
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
     chatbox.scrollTo(0, chatbox.scrollHeight);
-    chatbox.appendChild(incomingChatLi); // Menambahkan elemen <li> ke chatbox
-    await generateResponse(incomingChatLi);
-  }, 10);
+
+    setTimeout(async () => {
+        // Menampilkan delay bot untuk merespon pengguna
+        const incomingChatLi = createChatLi(
+            "Maaf saya tidak mengerti :)",
+            "incoming"
+        );
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+        chatbox.appendChild(incomingChatLi); // Menambahkan elemen <li> ke chatbox
+        await generateResponse(incomingChatLi);
+    }, 10);
 };
 
 chatInput.addEventListener("input", () => {
-  chatInput.style.height = `${inputInitHeight}px`;
-  chatInput.style.height = `${chatInput.scrollHeight}px`;
+    chatInput.style.height = `${inputInitHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
 });
 
 chatInput.addEventListener("keydown", (e) => {
-  if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
-    e.preventDefault();
-    handleChat();
-  }
+    if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleChat();
+    }
 });
 
 sendChatBtn.addEventListener("click", handleChat);
 chatbotCloseBtn.addEventListener("click", () =>
-  document.body.classList.remove("show-chatbot")
+    document.body.classList.remove("show-chatbot")
 );
 chatbotToggler.addEventListener("click", () =>
-  document.body.classList.toggle("show-chatbot")
+    document.body.classList.toggle("show-chatbot")
 );
--- static/js/script.js
+++ static/js/script.js
@@ -1,34 +1,77 @@
-// Load clients
-function loadClients() {
-    let clientListContainer = document.querySelector("#client-list");
-    let clientListElm = "";
-    let clientList = [
-        "access.svg",
-        "akr.webp",
-        "binakarir.jpg",
-        "ganex.jpg",
-        "gledex.jpg",
-        "indomarco.jpg",
-        "iskent.png",
-        "kenko.jpg",
-        "kerry.png",
-        "onecare.jpg",
-        "pbd.jpg",
-        "sinar.jpg",
-        "slank.jpg",
-        "spt.webp",
-        "tgs.png",
-        "ua.png",
-        "unilab.jpg",
-    ];
-
-    clientList.forEach((images) => {
-        clientListElm += `
-            <swiper-slide>
-                <img src="static/images/client_bce/${images}" alt="${images}" class="client-img" />
-            </swiper-slide>
-        `;
+// jQuery
+// Owl carousel
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoWidth: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true,
+            },
+            600: {
+                items: 3,
+                nav: false,
+            },
+            1000: {
+                items: 5,
+                nav: true,
+            },
+        },
     });
-    clientListContainer.innerHTML = clientListElm;
+});
+
+// dropdown
+const dropdown = document.querySelector(".dropdown");
+const dropdownmenu = document.querySelector(".dropdown-menu");
+
+dropdown.addEventListener("click", function () {
+    dropdownmenu.classList.toggle("show");
+});
+dropdownmenu.addEventListener("click", () => {
+    if (dropdownmenu.classList.contains("show")) {
+        dropdownmenu.classList.remove("show");
+    }
+});
+
+// Padding navigasi berubah ketika di scroll
+window.onscroll = function () {
+    let navigasi = document.querySelector(".navbar");
+
+    if (
+        document.body.scrollTop > 120 ||
+        document.documentElement.scrollTop > 120
+    ) {
+        navigasi.classList.add("scroll");
+    } else {
+        navigasi.classList.remove("scroll");
+    }
+};
+
+// Mobile menu
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCont = document.querySelectorAll(".menu-content li");
+const menuBtn = document.querySelector(".menu-btn");
+const closeMenuBtn = document.querySelector(".close-menu-btn");
+
+function openMenu() {
+    if (!mobileMenu.classList.contains("show")) {
+        mobileMenu.classList.add("show");
+    }
 }
-loadClients();
+
+function closeMenu() {
+    if (mobileMenu.classList.contains("show")) {
+        mobileMenu.classList.remove("show");
+    }
+}
+
+menuBtn.addEventListener("click", openMenu);
+closeMenuBtn.addEventListener("click", closeMenu);
+menuCont.forEach((menu) => {
+    menu.addEventListener("click", closeMenu);
+});
